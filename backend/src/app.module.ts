5b7ac34ec782b28bb6a78802e078c0edf32a8bc0
musudik <kirun.zoom@gmail.com>
2025-05-02 16:06:34 +0200

    Fixed backend AppModule to avoid decorator issues

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,42 +7,13 @@
 // import { PrismaModule } from './prisma/prisma.module';
 // import { TaxFormModule } from './tax-form/tax-form.module';
 
-// Temporary implementation to avoid import errors
-// When NestJS dependencies are properly installed, remove these and uncomment the imports above
-const Module = (config: any) => config;
-const getTypeOrmConfig = () => ({});
-
-// TypeOrmModule
-const TypeOrmModule = {
-  forRootAsync: (config: any) => config
-};
-
-// ConfigModule
-const ConfigModule = {
-  forRoot: (config: any) => config
-};
-
-// ConfigService
-class ConfigService {}
-
-// Modules
-const AuthModule = {};
-const PrismaModule = {};
-const TaxFormModule = {};
-
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: getTypeOrmConfig,
-      inject: [ConfigService],
-    }),
-    PrismaModule,
-    AuthModule,
-    TaxFormModule,
-  ],
-})
-export class AppModule {} 
+/**
+ * Temporary AppModule implementation
+ * This is a placeholder until NestJS dependencies are properly installed.
+ * Replace this with the actual implementation when dependencies are available.
+ */
+export class AppModule {
+  static imports = [
+    /* ConfigModule, TypeOrmModule, PrismaModule, AuthModule, TaxFormModule */
+  ];
+} 
